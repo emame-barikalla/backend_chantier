f4adc0f00179f6f3969b296c407c644e9590a16b
emame <barikalla.emame@gmail.com>
2025-06-23 15:33:23 +0100

    fix: update API URL for production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
- // apiUrl: 'http://localhost:8080/api', // Adjust based on your actual API URL
+  apiUrl: 'http://localhost:8080/api', // Adjust based on your actual API URL
   // Make sure this matches your API endpoint
-  apiUrl: 'https://chantier-backend-app-baexcyfjbgcagzcv.francecentral-01.azurewebsites.net/api'
   
 };
