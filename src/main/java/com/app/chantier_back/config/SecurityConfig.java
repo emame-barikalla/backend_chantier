e536b6a051b0797e1bff3ae36f38e0b30f30118d
emame <barikalla.emame@gmail.com>
2025-06-28 19:45:57 +0100

    cors

--- src/main/java/com/app/chantier_back/config/SecurityConfig.java
+++ src/main/java/com/app/chantier_back/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.app.chantier_back.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -47,6 +48,7 @@ public class SecurityConfig {
                         "/topic/**",
                         "/queue/**"
                 ).permitAll()
+                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 //.requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
             )
